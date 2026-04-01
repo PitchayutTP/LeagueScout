f06e9ba48703f73668c5dc51b87342df1ab18bf1
PitchayutTP <66070133@kmitl.ac.th>
2026-04-02 03:34:11 +0700

    merge discovery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Prospects from "./pages/Prospects";
+import DiscoveryDashboard from "./pages/DiscoveryDashboard";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/Prospects" element={<Prospects />} />
+        <Route path="/player-search" element={<DiscoveryDashboard />} />
       </Routes>
+      
   );
 }
 
